a180e3b56e4679184e89891163956b5edc0cee35
Tommy Sneltvedt <251251@student.usn.no>
2025-04-29 02:28:07 +0200

    Add prbs test
    Added test of pseudo random binary signal (PRBS) function. The function itself, prbs1(), was developed by Dr. David Di Ruscio.

---
+++ Master/DP model/System Identification/test_prbs.m
@@ -0,0 +1,25 @@
+% Simple script for testing the prbs1() function.
+% PRBS: Pseudo Random Binary Signal.
+% This function was developed by David Di Ruscio.
+clear, clc, close all;
+
+addpath("..\..\Tools\");
+
+Tmin = 50;      % Maximum interval of constant signal
+Tmax = 150;     % Minimum interval of constant signal
+N = 2000;       % Number of samples
+t = 1:1:2000;   % Time of samples
+
+[u, ~] = prbs1(N, Tmin, Tmax);
+
+% Plot PRBS signal
+fig = figure(1);
+plot(t, u);
+grid();
+title('Pseudo Random Binary Signal (PRBS)');
+ylim([-1.2, 1.2]);
+xlabel('t [time]');
+ylabel('u [signal]');
+
+% Store plot
+save_plot(fig, "PRBS_example", "Results/PRBS");
